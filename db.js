318706df7ba43de86c8fd79de054f5fd222c1428
maxus_red <incineroaraditya@gmail.com>
2024-05-31 00:17:25 +0530

    fixed minor changes

--- db.js
+++ db.js
@@ -10,9 +10,6 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-
-
 }
 
 const contact = new mongoose.Schema({
